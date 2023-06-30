1f2409c1d154d84b150ed1166e40b5ded703ac5b
maen <maen210212@gmail.com>
2023-06-30 16:47:49 -0600

    Project creation.

---
+++ src/main/java/com/google/SpringBootRecaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecaptchaApplication.class, args);
+	}
+
+}
